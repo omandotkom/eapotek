9e7a45a8a8cf3e1318ed757a030f196323fc9abc
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-17 19:11:11 +0700

    bikin halaman input-group bootstrap

--- resources/views/input/obat.blade.php
+++ resources/views/input/obat.blade.php
@@ -3,18 +3,65 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Tambah / Edit Data Obat</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Kode Obat</span>
                         </div>
-                    @endif
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
 
-                    You are logged in! {{ Auth::user()->name }}
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Cabang</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Nama</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="inputGroupSelect01">Bentuk</label>
+                        </div>
+                        <select class="custom-select" id="inputGroupSelect01">
+                            <option selected>Pilih</option>
+                            <option value="1">Tablet</option>
+                            <option value="2">Kapsul</option>
+                            <option value="3">Kaplet</option>
+                            <option value="3">Puyer</option>
+                            <option value="3">Larutan</option>
+                            <option value="3">Suspensi</option>
+                            <option value="3">Eliksir</option>
+                            <option value="3">Injeksi</option>
+                            <option value="3">Infus</option>
+                            <option value="3">Salep</option>
+                        </select>
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Harga Obat</span>
+                            <span class="input-group-text">Rp</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Stok</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
                 </div>
             </div>
         </div>
