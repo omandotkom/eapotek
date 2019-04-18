7172e04cd5b04ef65b31e476b4ff6e2c94a1b387
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-18 09:18:51 +0700

    modify email to nik

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('role');
-            $table->string('email')->unique();
+            $table->string('nik')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
