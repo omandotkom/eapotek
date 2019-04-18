defadda7f428f390f581a16591c7b35c61eb8a53
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-18 09:19:41 +0700

    Add user table seeder

---
+++ database/seeds/User_Table_Seeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class User_Table_Seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Khalid Abdurrahman',
+            'nik' => '16102126',
+            'role' => 'branchhead',
+            'password' => Hash::make('system3298'),
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'Ridwan Risandi',
+            'nik' => '16102137',
+            'role' => 'owner',
+            'password' => Hash::make('system3298'),
+        ]);
+        
+        DB::table('users')->insert([
+            'name' => 'Hesa Abda',
+            'nik' => '16102124',
+            'role' => 'branchworker',
+            'password' => Hash::make('system3298'),
+        ]);
+    }
+}
