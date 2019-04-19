8aa5337fa06e620ec6260e5450f6d51448a9d2d1
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-19 09:13:46 +0700

    add fillable column

--- app/Supplier.php
+++ app/Supplier.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     protected $table = 'suppliers';
+    protected $fillable = ['branch_id','nama','alamat','telepon','email'];
 }
