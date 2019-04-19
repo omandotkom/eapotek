babe1e4c51cbe6b597943230c0b72c9a817c1eae
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-19 09:15:05 +0700

    change from inputgroup to form

--- resources/views/input/obat.blade.php
+++ resources/views/input/obat.blade.php
@@ -8,60 +8,49 @@
                 <div class="card-header">Tambah / Edit Data Obat</div>
 
                 <div class="card-body">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-default">Kode Obat</span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                    </div>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-default">Cabang</span>
+                    <form>
+                        <div class="form-group">
+                            <label for="kodeObat">Kode Obat</label>
+                            <input type="text" class="form-control" id="kodeObat" placeholder="BTK2">
                         </div>
-                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                    </div>
+                        <div class="form-group">
+                            <label for="namaObat">Nama Obat</label>
+                            <input type="text" class="form-control" id="namaObat" placeholder="OBH Combine">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputState">Bentuk</label>
+                            <select id="inputState" class="form-control">
+                                <option value="1">Tablet</option>
+                                <option value="2">Kapsul</option>
+                                <option value="3">Kaplet</option>
+                                <option value="3">Puyer</option>
+                                <option value="3">Larutan</option>
+                                <option value="3">Suspensi</option>
+                                <option value="3">Eliksir</option>
+                                <option value="3">Injeksi</option>
+                                <option value="3">Infus</option>
+                                <option value="3">Salep</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="hargaObat">Harga Obat</label>
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Rp</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Default"  placeholder="32.000" aria-describedby="inputGroup-sizing-default">
+                            </div>   
+                        </div>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-default">Nama</span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                    </div>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <label class="input-group-text" for="inputGroupSelect01">Bentuk</label>
-                        </div>
-                        <select class="custom-select" id="inputGroupSelect01">
-                            <option selected>Pilih</option>
-                            <option value="1">Tablet</option>
-                            <option value="2">Kapsul</option>
-                            <option value="3">Kaplet</option>
-                            <option value="3">Puyer</option>
-                            <option value="3">Larutan</option>
-                            <option value="3">Suspensi</option>
-                            <option value="3">Eliksir</option>
-                            <option value="3">Injeksi</option>
-                            <option value="3">Infus</option>
-                            <option value="3">Salep</option>
-                        </select>
-                    </div>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-default">Harga Obat</span>
-                            <span class="input-group-text">Rp</span>
+                        <div class="form-group">
+                            <label for="stokObat">Stok Obat</label>
+                            <input type="text" class="form-control" id="stokObat" placeholder="">
                         </div>
-                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                    </div>
+                    </form>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroup-sizing-default">Stok</span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                    </div>
                 </div>
             </div>
         </div>
