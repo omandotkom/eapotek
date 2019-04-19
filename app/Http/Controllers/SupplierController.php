23256ba9fc354b6297f421dca51eeddc4e70af7c
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-19 09:13:27 +0700

    add store() function but not working

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Branch;
+use App\Supplier;
+use Illuminate\Support\Facades\Auth;
 class SupplierController extends Controller
 {
-    //
+    
+    public function showInputSupplierView(){
+        $cabang = Branch::where('nik',Auth::user()->nik)->first();
+        
+        return view('input.supplier',['cabang'=>$cabang]);
+    }
+    public function store(Request $request){
+       $data = $request->all();
+       $supplier = new Supplier($data);
+       $result = $transaction->isValid();
+       echo $result;
+    }
 }
