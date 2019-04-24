5ce26a45eefaf474cb690dcd1871adaa4364ed91
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-24 15:02:19 +0700

    add view obat

---
+++ resources/views/view/obat.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.logged')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">Lihat Data Obat</div>
+                <!-- tulis kode disini-->
+
+                
+                <!--sampai sini-->
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
