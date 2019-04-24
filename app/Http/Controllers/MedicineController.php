f1d99f1ba0bd578c285ffb13600436cbd4928037
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-24 16:03:52 +0700

    MedicineController now has showInputObatView

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Branch;
+use App\Supplier;
+use Illuminate\Support\Facades\Auth;
 class MedicineController extends Controller {
 
     public function showInputObatView() {
         $cabang = Branch::where('nik', Auth::user()->nik)->first();
-
-        return view('input.obat', ['cabang' => $cabang]);
+        $supplier = Supplier::where('branch_id',$cabang->id)->get(); 
+        return view('input.obat', ['cabang' => $cabang,'supplier'=> $supplier]);
     }
 
 }
