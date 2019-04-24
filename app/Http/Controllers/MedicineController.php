0c3467026c07a17c36d597adecb699349134fdaa
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-24 11:35:50 +0700

    add showInputObatView

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Branch;
+class MedicineController extends Controller {
 
-class MedicineController extends Controller
-{
-    //
+    public function showInputObatView() {
+        $cabang = Branch::where('nik', Auth::user()->nik)->first();
+
+        return view('input.obat', ['cabang' => $cabang]);
+    }
+
 }
