d213734bdf2014cb74aacc6435ab9bb1606fad74
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-24 11:35:27 +0700

    add fillable field

--- app/Medicine.php
+++ app/Medicine.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Medicine extends Model
 {
     protected $table = 'medicines';
+    protected $fillable = ['id','branch_id','supplier_id','namaobat','satuan','harga','stok','updated_at'];
 }
