d21b9a88bdc9043e113dad113e539124c2c64b29
Gocir <ridwanwanwan72@gmail.com>
2019-04-24 14:22:35 +0700

    Membuat halaman karyawan

--- resources/views/input/karyawan.blade.php
+++ resources/views/input/karyawan.blade.php
@@ -6,42 +6,47 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Absensi</div>
+                <div class="card-header">Tambah / Edit Data Karyawan</div>
 
                 <div class="card-body">
                     <form id="formSupplier" method="post" action="javascript:void(0)">
                         <fieldset disabled>
                             <div class="form-group">
-                                <label for="kodeAttendance">Kode Absensi</label>
-                                <input type="text" id="kodeAttendance" value="" class="form-control" placeholder="Disabled input">
+                                <label for="nik">NIK</label>
+                                <input type="text" class="form-control" name="nik" id="kontak" placeholder="16102126">
                             </div>
                         </fieldset>
                         <div class="form-group">
-                            <label for="nik">NIK</label>
-                            <input type="text" class="form-control" name="nik" id="kontak" placeholder="16102126">
+                            <label for="nama">Nama Lengkap</label>
+                            <input type="text" class="form-control" name="nama" id="kontak" placeholder="Khalid Abdurrahman">
                         </div>
                         <div class="form-group">
-                            <label for="tanggalSupply">Tanggal</label>
-                            <input type="date" id="inputMDEx" class="form-control">
+                            <label for="alamat">Alamat</label>
+                            <textarea class="form-control" id="alamat" name="alamat" rows="3" placeholder="Lengkap beserta kecamatan, kabupaten, provinsi hingga kode pos"></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="keterangan">Keterangan</label>
+                            <label for="nomorKTP">Nomor KTP</label>
+                            <input type="text" class="form-control" name="nomorKTP" id="kontak" placeholder="+62 ....">
                         </div>
-                        <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadioInline1">Masuk</label>
+                        <div class="form-group">
+                            <label for="kodeCabang">Kode Cabang</label>
+                            <select id="kodeCabang" class="form-control">
+                                <option selected>Pilih ..</option>
+                                <option value="1">CBG001</option>
+                                <option value="2">CBG002</option>
+                                <option value="3">CBG003</option>
+                                <option value="4">CBG004</option>
+                            </select> 
                         </div>
-                        <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadioInline2">Izin</label>
-                        </div>
-                        <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadioInline3">Alpha</label>
-                        </div>
-                        <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadioInline4">Cuti</label>
+                        <div class="form-group">
+                            <label for="kodeJabatan">Kode Jabatan</label>
+                            <select id="kodeCabang" class="form-control">
+                                <option selected>Pilih ..</option>
+                                <option value="1">Kepala Cabang</option>
+                                <option value="2">Kasir</option>
+                                <option value="3">Apoteker</option>
+                                <option value="4">Dokter</option>
+                            </select> 
                         </div>
                         <div class="text-right">
                             <button type="submit" onclick="onSubmitClicked();" id="send_form" class="btn btn-outline-success">Simpan</button>
