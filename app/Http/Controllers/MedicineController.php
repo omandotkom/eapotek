1fd923043fe2d4c70ffdf4fc90664f3c7fd91814
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-25 14:26:49 +0700

    add store function to save medicine data

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -6,12 +6,31 @@ use Illuminate\Http\Request;
 use App\Branch;
 use App\Supplier;
 use Illuminate\Support\Facades\Auth;
+use App\Medicine;
+use App\Supplying;
+
 class MedicineController extends Controller {
 
     public function showInputObatView() {
         $cabang = Branch::where('nik', Auth::user()->nik)->first();
-        $supplier = Supplier::where('branch_id',$cabang->id)->get(); 
-        return view('input.obat', ['cabang' => $cabang,'supplier'=> $supplier]);
+        $supplier = Supplier::where('branch_id', $cabang->id)->get();
+        return view('input.obat', ['cabang' => $cabang, 'supplier' => $supplier]);
+    }
+
+    public function store(Request $request) {
+        $medicine = new Medicine;
+        $medicine->id = $request->id;
+        $medicine->branch_id = $request->branch_id;
+        $medicine->namaobat = $request->namaobat;
+        $medicine->satuan = $request->satuan;
+        $medicine->stok = $request->stok;
+        $medicine->harga = $request->harga;
+        $medicine->save();
+        $supplying = Supplying::firstOrCreate(['id_supplier' => $request->id_supplier, 'id_medicine' => $request->id_medicine]);
+        return response()->json([
+                    'status' => 'success',
+                    'code' => 'storeMedicine'
+        ]);
     }
 
 }
