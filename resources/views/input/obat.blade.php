d88ead700a28507966a537384861619089e19964
Khalid Abdurrahman <omandotkom@gmail.com>
2019-04-25 14:29:04 +0700

    -add submit async save method using axios -success notification when successfully save medicine data

--- resources/views/input/obat.blade.php
+++ resources/views/input/obat.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="card-body">
 
-                    <form>
+                    <form id="formMedicine">
                         <div class="form-group">
                             <label for="kodeObat">Kode Obat</label>
                             <input type="text" class="form-control" id="kodeObat" placeholder="BTK2">
@@ -21,27 +21,28 @@
 
                         <div class="form-group">
                             <label for="namaObat">Supplier</label>
-                            <select class="form-control">
+                            <select id="supplier" class="form-control">
                                 @foreach($supplier as $sp)
                                 <option value="{{$sp->id}}">{{$sp->nama}}</option>
                                 @endforeach
-                               
-                                
+
+
                             </select>   
                         </div>
                         <div class="form-group">
                             <label for="inputState">Satuan</label>
-                            <select id="inputState" class="form-control">
-                                <option value="1">Tablet</option>
-                                <option value="2">Kapsul</option>
-                                <option value="3">Kaplet</option>
-                                <option value="3">Puyer</option>
-                                <option value="3">Larutan</option>
-                                <option value="3">Suspensi</option>
-                                <option value="3">Eliksir</option>
-                                <option value="3">Injeksi</option>
-                                <option value="3">Infus</option>
-                                <option value="3">Salep</option>
+                            <select id="satuan" class="form-control">
+                                <option value="Tablet">Tablet</option>
+                                <option value="Kapsul">Kapsul</option>
+                                <option value="Kaplet">Kaplet</option>
+                                <option value="Puyer">Puyer</option>
+                                <option value="Larutan">Larutan</option>
+                                <option value="Suspensi">Suspensi</option>
+                                <option value="ELiksir">Eliksir</option>
+                                <option value="Injeksi">Injeksi</option>
+                                <option value="Infus">Infus</option>
+                                <option value="Salep">Salep</option>
+                                <option value="Satuan Lain">Lainnya</option>
                             </select>
                         </div>
                         <div class="form-group">
@@ -50,16 +51,73 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">Rp</span>
                                 </div>
-                                <input type="text" class="form-control" aria-label="Default"  placeholder="32.000" aria-describedby="inputGroup-sizing-default">
+                                <input type="text" id="harga" class="form-control" aria-label="Default"  placeholder="32.000" aria-describedby="inputGroup-sizing-default">
                             </div>   
                         </div>
                         <div class="form-group">
                             <label for="stokObat">Stok Obat</label>
                             <input type="text" class="form-control" id="stokObat" placeholder="">
                         </div>
-                         <div class="text-right">
-                            <button type="submit" onclick="onSubmitClicked();" id="send_form" class="btn btn-outline-success">Simpan</button>
+                        <div class="text-right">
+                            <button type="submit"  id="send_form" class="btn btn-outline-success">Simpan</button>
                         </div>
+                        <script>
+                            jQuery(document).ready(function () {
+                                jQuery('#send_form').click(function (e) {
+                                    e.preventDefault();
+                                    $.ajaxSetup({
+                                        headers: {
+                                            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                                        }
+                                    });
+                                    $("#send_form").html('Menyimpan...');
+                                    axios.post('http://homestead.test/input/obat/store', {
+                                        id: jQuery('#kodeObat').val(),
+                                        id_medicine: jQuery('#kodeObat').val(),
+                                        branch_id: '{{$cabang->id}}',
+                                        namaobat: jQuery('#namaObat').val(),
+                                        id_supplier: jQuery('#supplier').val(),
+                                        satuan: jQuery('#satuan').val(),
+                                        harga: jQuery('#harga').val(),
+                                        email: jQuery('#email').val(),
+                                        stok: jQuery('#stokObat').val()
+
+                                    })
+                                            .then(function (response) {
+                                                console.log(response);
+                                                $("#send_form").html('Simpan');
+                                                toastr.options = {
+                                                    "closeButton": false,
+                                                    "debug": false,
+                                                    "newestOnTop": false,
+                                                    "progressBar": false,
+                                                    "positionClass": "toast-top-center",
+                                                    "preventDuplicates": false,
+                                                    "onclick": null,
+                                                    "showDuration": "300",
+                                                    "hideDuration": "1000",
+                                                    "timeOut": "5000",
+                                                    "extendedTimeOut": "1000",
+                                                    "showEasing": "swing",
+                                                    "hideEasing": "linear",
+                                                    "showMethod": "fadeIn",
+                                                    "hideMethod": "fadeOut"
+                                                };
+                                                Command: toastr["success"]("Berhasil menyimpan data", "Berhasil");
+                                            })
+                                            .catch(function (error) {
+                                                //$("#send_form").html('Simpan',"error");
+                                                console.log(error);
+                                            });
+                                });
+                            });
+
+
+
+
+
+
+                        </script>
                     </form>
 
                 </div>
