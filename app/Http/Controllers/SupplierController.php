37c5ec5470433692925a87538cab29af4a4e457c
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-13 21:05:21 +0700

    add save (store)

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -15,18 +15,30 @@ class SupplierController extends Controller {
         return view('input.supplier', ['cabang' => $cabang]);
     }
 
-    public function store(Request $request) {
-        $data = $request->all();
-        $supplier = new Supplier($data);
-        $result = $transaction->isValid();
-        echo $result;
-    }
-    
-    public function showSupplierByBranch(Request $request){
-        $supplier = Supplier::where('branch_id',$request->branch_id)
-                ->orderBy('nama','asc')
+    /* public function store(Request $request) {
+      $data = $request->all();
+      $supplier = new Supplier($data);
+      $result = $transaction->isValid();
+      echo $result;
+      } */
+
+    public function showSupplierByBranch(Request $request) {
+        $supplier = Supplier::where('branch_id', $request->branch_id)
+                ->orderBy('nama', 'asc')
                 ->get();
-        return (string ) $supplier;
+        return (string) $supplier;
     }
 
+    public function store(Request $request) {
+        $supplier = new Supplier;
+        $supplier->branch_id = $request->branch_id;
+        $supplier->nama = $request->nama;
+        $supplier->alamat = $request->alamat;
+        $supplier->telepon = $request->telepon;
+        $supplier->email = $request->email;
+        $supplier->save();
+        return Response::json([
+                    'action' => 'save_supplier'
+                        ], 201); // Status code here
+    }
 }
