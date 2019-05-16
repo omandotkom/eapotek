2012fba5a0ae1b57b762aefebf0a44696d1477f8
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-16 13:39:08 +0700

    create TransactionController

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+   public function Store(Request $request){
+       
+       
+   }
+}
