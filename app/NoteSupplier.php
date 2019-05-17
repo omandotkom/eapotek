64b8f90900cf45a4de4906fe19111c2847026407
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-17 10:54:05 +0700

    NoteSupplier table, disabled timestamp

---
+++ app/NoteSupplier.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NoteSupplier extends Model
+{
+    protected $table = 'note_supplier';
+    public $timestamps = false;
+    
+}
