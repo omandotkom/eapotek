04c28c63085dba9dcfc10f23ac22146099f68e00
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-17 19:26:17 +0700

    create migration file for worker table

---
+++ database/migrations/2019_05_17_112000_create_workers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->string('nik',10)->primary();
+            $table->integer('branch_id');
+            $table->string('nama',30);
+            $table->text('alamat');
+            $table->string('noktp',20);
+            $table->integer('position_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}
