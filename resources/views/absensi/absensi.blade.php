dc9b2f1ba90267296e5ffd5a14691129f58b1e01
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-18 14:49:02 +0700

    connect to backend

--- resources/views/absensi/absensi.blade.php
+++ resources/views/absensi/absensi.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.logged')
 
 @section('content')
 
@@ -10,44 +10,88 @@
 
                 <div class="card-body">
                     <form id="formSupplier" method="post" action="javascript:void(0)">
-                        <fieldset disabled>
+                       <fieldset disabled>
                             <div class="form-group">
-                                <label for="kodeAttendance">Kode Absensi</label>
-                                <input type="text" id="kodeAttendance" value="" class="form-control" placeholder="Disabled input">
+                                <label for="namaCabang">Cabang</label>
+                                <input type="text" id="namaCabang" value="{{$cabang->nama}}" class="form-control" placeholder="Disabled input">
                             </div>
                         </fieldset>
                         <div class="form-group">
-                            <label for="nik">NIK</label>
-                            <input type="text" class="form-control" name="nik" id="kontak" placeholder="16102126">
+                             <label for="nik">Karyawan</label>
+                            <select id="nik" class="form-control">
+                                @foreach($worker as $w)
+                                <option value="{{$w->nik}}">{{$w->nama}}</option>
+                                @endforeach
+
+
+                            </select>   
                         </div>
+                       
                         <div class="form-group">
                             <label for="tanggalSupply">Tanggal</label>
-                            <input type="date" id="inputMDEx" class="form-control">
+                            <input type="date" id="tanggal" class="form-control">
                         </div>
                         <div class="form-group">
                             <label for="keterangan">Keterangan</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+                            <input type="radio" id="customRadioInline1" name="keterangan" value ="Masuk" class="custom-control-input">
                             <label class="custom-control-label" for="customRadioInline1">Masuk</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+                            <input type="radio" id="customRadioInline2" name="keterangan" value ="Izin" class="custom-control-input">
                             <label class="custom-control-label" for="customRadioInline2">Izin</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline3" name="customRadioInline1" class="custom-control-input">
+                            <input type="radio" id="customRadioInline3" name="keterangan" value ="Alpha" class="custom-control-input">
                             <label class="custom-control-label" for="customRadioInline3">Alpha</label>
                         </div>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="customRadioInline4" name="customRadioInline1" class="custom-control-input">
+                            <input type="radio" id="customRadioInline4" name="keterangan" value ="Cuti" class="custom-control-input">
                             <label class="custom-control-label" for="customRadioInline4">Cuti</label>
                         </div>
                         <div class="text-right">
                             <button type="submit" onclick="onSubmitClicked();" id="send_form" class="btn btn-outline-success">Simpan</button>
                         </div>
                         <script>
+                            async function onSubmitClicked() {
 
+                                $("#send_form").html('Menyimpan...');
+                                axios.post('http://homestead.test/absensi/store', {
+                                    nik: jQuery('#nik').val(),
+                                    tanggal: jQuery('#tanggal').val(),
+                                    keterangan: jQuery("input[name='keterangan']:checked").val()
+
+                                })
+                                        .then(function (response) {
+                                            toastr.options = {
+                                                "closeButton": false,
+                                                "debug": false,
+                                                "newestOnTop": false,
+                                                "progressBar": false,
+                                                "positionClass": "toast-top-center",
+                                                "preventDuplicates": false,
+                                                "onclick": null,
+                                                "showDuration": "300",
+                                                "hideDuration": "1000",
+                                                "timeOut": "5000",
+                                                "extendedTimeOut": "1000",
+                                                "showEasing": "swing",
+                                                "hideEasing": "linear",
+                                                "showMethod": "fadeIn",
+                                                "hideMethod": "fadeOut"
+                                            };
+                                            Command: toastr["success"]("Berhasil menyimpan data", "Berhasil");
+
+                                            console.log(response);
+                                            $("#send_form").html('Simpan');
+                                        })
+                                        .catch(function (error) {
+                                            toastr.error("Gagal menyimpan data", "Kesalahan");
+                                            $("#send_form").html('Simpan');
+                                            console.log(error);
+                                        });
+                            }
                         </script>
                     </form>
                 </div>
