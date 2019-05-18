44d63a4a2bcdf9c84143bf3afe83c55736fb47f6
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-18 14:48:44 +0700

    create attendance table

---
+++ database/migrations/2019_05_17_125423_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nik',10);
+            $table->date('tanggal');
+            $table->string('keterangan',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
