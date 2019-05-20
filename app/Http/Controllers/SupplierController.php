657dc64a0f411356693eb4fadff231fbf6c36ecd
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-20 16:58:51 +0700

    add showAllSupplier function

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -7,12 +7,12 @@ use App\Branch;
 use App\Supplier;
 use Illuminate\Support\Facades\Auth;
 use Response;
+use DB;
 class SupplierController extends Controller {
 
     public function showInputSupplierView() {
         $cabang = Branch::where('nik', Auth::user()->nik)->first();
-
-        return view('input.supplier', ['cabang' => $cabang]);
+       
     }
 
     /* public function store(Request $request) {
@@ -41,4 +41,12 @@ class SupplierController extends Controller {
                     'action' => 'save_supplier'
                         ], 201); // Status code here
     }
+    
+    public function showAllSupplier(){
+        $suppliers = Supplier::with('branch')->simplePaginate(10);
+        //$suppliers = DB::table("suppliers")->simplePaginate(10);
+        $branch = Branch::all();
+        return view('view.supplier',['suppliers' => $suppliers,'branches'=>$branch]);
+    }
+    
 }
