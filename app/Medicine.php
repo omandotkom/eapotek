6631701bffe9d26e1fd76c51844c84841889a5b5
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-20 16:58:21 +0700

    add medicine relation

--- app/Medicine.php
+++ app/Medicine.php
@@ -8,5 +8,8 @@ class Medicine extends Model
 {
     protected $table = 'medicines';
     protected $fillable = ['id','branch_id','namaobat','satuan','harga','stok'];
+    public function branch(){
+        return $this->hasOne('App\Branch','id','branch_id');
+    }
     
 }
