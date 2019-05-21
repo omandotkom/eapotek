f35d056d3627611b635488551ab2c4f086b25981
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-21 13:05:02 +0700

    set increment to false & add hasOne branch

--- app/Medicine.php
+++ app/Medicine.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Medicine extends Model
 {
     protected $table = 'medicines';
+    public $incrementing = false;
     protected $fillable = ['id','branch_id','namaobat','satuan','harga','stok'];
     public function branch(){
         return $this->hasOne('App\Branch','id','branch_id');
