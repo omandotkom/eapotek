8507ad9754d7747f4a5081cfe42b0fb6477e312a
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-21 10:36:37 +0700

    simplify design

--- resources/views/view/supplyobat.blade.php
+++ resources/views/view/supplyobat.blade.php
@@ -31,7 +31,6 @@
                                 <th scope="col">Kode Note</th>
                                 <th scope="col">Cabang</th>
                                 <th scope="col">Supplier</th>
-                                <th scope="col">Deskripsi</th>
                                 <th scope="col">Total Biaya</th>
                                 <th scope="col">Tanggal</th>
                             </tr>
@@ -42,55 +41,10 @@
                                 <td>NS001</td>
                                 <td>Bekasi</td>
                                 <td>Adinda Miftahul Ilmi Habiba</td>
-                                <td>OBH Combi 20 botol</td>
                                 <td>Rp 1.500.000</td>
                                 <td>26/04/2019</td>
                             </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>NS002</td>
-                                <td>Bekasi2</td>
-                                <td>Hesa Abda Arrasyid</td>
-                                <td>Bodrexin 50 botol</td>
-                                <td>Rp 5.000.000</td>
-                                <td>26/04/2019</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>NS003</td>
-                                <td>Bekasi1</td>
-                                <td>Khalid Abdurrahman</td>
-                                <td>Paramex 100 pcs</td>
-                                <td>Rp 3.000.000</td>
-                                <td>27/04/2019</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td>NS004</td>
-                                <td>Purwokerto</td>
-                                <td>Lela Sari Kristina</td>
-                                <td>Procold 10 pcs</td>
-                                <td>Rp 100.000</td>
-                                <td>27/04/2019</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">5</th>
-                                <td>NS005</td>
-                                <td>Yogyakarta</td>
-                                <td>Muhammad Ilham Hijriyanta</td>
-                                <td>Koyo 75 bungkus</td>
-                                <td>Rp 2.000.000</td>
-                                <td>29/04/2019</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">6</th>
-                                <td>NS006</td>
-                                <td>Purwokerto</td>
-                                <td>Ridwan Risandi</td>
-                                <td>Promaag 20 pcs</td>
-                                <td>Rp 200.000</td>
-                                <td>29/04/2019</td>
-                            </tr>
+                            
                         </tbody>
                     </table>
 
