e420426e77b55c3f1a6f219d3af876ec0ac27ed3
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-21 10:36:23 +0700

    simplify design

--- resources/views/view/karyawan.blade.php
+++ resources/views/view/karyawan.blade.php
@@ -30,10 +30,7 @@
                                 <th scope="col">No.</th>
                                 <th scope="col">Cabang</th>
                                 <th scope="col">NIK</th>
-                                <th scope="col">Nomor KTP</th>
                                 <th scope="col">Nama</th>
-                                <th scope="col">Alamat</th>
-                                <th scope="col">Nomor HP</th>
                                 <th scope="col">Jabatan</th>
                             </tr>
                         </thead>
@@ -42,62 +39,10 @@
                                 <th scope="row">1</th>
                                 <td>Bekasi1</td>
                                 <td>16102109</td>
-                                <td>3216102109960009</td>
                                 <td>Adinda Miftahul Ilmi Habiba</td>
-                                <td>Perum. Taman Gading Blok E10 Jl. Pajajaran RT/RW 09/09, Kecamatan Lain, Kabupaten Cilacap, Jawa Tengah - Indonesia 14022</td>
-                                <td>+62 82133155162</td>
                                 <td>Apoteker</td>
                             </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Bekasi2</td>
-                                <td>16102124</td>
-                                <td>3216102124960009</td>
-                                <td>Hesa Abda Arrasyid</td>
-                                <td>Tawang Sari, Kecamatan Mana, Wonosobo, Jawa Tengah - Indonesia 1200</td>
-                                <td>+62 85729194007</td>
-                                <td>Apoteker</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Bekasi1</td>
-                                <td>16102126</td>
-                                <td>3216102126960009</td>
-                                <td>Khalid Abdurrahman</td>
-                                <td>Perum. Telaga Harapan Blok D9/4, Kecamatan Cikarang Barat, Kabupaten Bekasi, Jawa Tengah - Indonesia 1945</td>
-                                <td>+62 81393558430</td>
-                                <td>Kepala Cabang</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td>Purwokerto</td>
-                                <td>16102127</td>
-                                <td>3216102127960009</td>
-                                <td>Lela Sari Kristina</td>
-                                <td>Perum. Bandengan Indah A7 RT/RW 01/16, Kecamatan juga, Cilacap, Jawa Tengah - Indonesia 15347</td>
-                                <td>+62 85875104418</td>
-                                <td>Dokter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">5</th>
-                                <td>Yogyakarta</td>
-                                <td>16102131</td>
-                                <td>3216102131960009</td>
-                                <td>Muhammad Ilham Hijriyanta</td>
-                                <td>Pakajangan Gang 14 No.46 RT/RW 33/12, Kecamatan Kedungwuni, Kabupaten Pekalongan, Jawa Tengah - Indonesia 14045</td>
-                                <td>+62 85600116266</td>
-                                <td>Kasir</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">6</th>
-                                <td>Purwokerto</td>
-                                <td>16102137</td>
-                                <td>3216063110960009</td>
-                                <td>Ridwan Risandi</td>
-                                <td>Perum. Bumi Sani Permai Blok B7/8 RT/RW 010/014, Kelurahan Setia Mekar, Kecamatan Tambun Selatan, Kabupaten Bekasi, Jawa Barat - Indonesia 17510</td>
-                                <td>+62 89685802629</td>
-                                <td>Kepala Cabang</td>
-                            </tr>
+                            
                         </tbody>
                     </table>
 
