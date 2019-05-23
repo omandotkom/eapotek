bd7566e6fce7d34490d5072f8d9a12a1e743bc60
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-23 09:18:11 +0700

    create workers table seeder

---
+++ database/seeds/WorkerSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+class WorkerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $faker = Faker::create();
+        for ($i = 1; $i <= 80; $i++) {
+
+            DB::table('workers')->insert([
+                'branch_id' => $faker->numberBetween($min = 1, $max = 4),
+                'nama' => $faker->name,
+                'alamat' => $faker->address,
+                'nik' => $faker->numerify('##########'), // 'Hello 609', // 79907610
+                'noktp' => $faker->creditCardNumber,
+                'position_id' => $faker->numberBetween($min = 1, $max = 30),
+                
+            ]);
+        }
+    }
+}
