24ea4ea20c1e657e396cafbe08ec7c1acf565a8d
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-23 09:17:06 +0700

    create positions table migration

---
+++ database/migrations/2019_05_21_062454_create_positions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('positions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('branch_id');
+            $table->string('nama');
+            $table->double('salary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('positions');
+    }
+}
