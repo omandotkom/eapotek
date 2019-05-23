1c8399f1ad6eac5743b0d30b2f348d8bcf410d46
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-23 09:16:49 +0700

    add branch() and position()

--- app/Worker.php
+++ app/Worker.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Worker extends Model
 {
      protected $table = 'workers';
-   
+     public function branch(){
+        return $this->hasOne('App\Branch','id','branch_id');
+    }
+    public function position(){
+        return $this->hasOne('App\Position','id','position_id');
+    }
+
 }
