96c956a0f98780791615e28178ba4c6a0667529c
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-23 09:17:35 +0700

    create positions table seeder

---
+++ database/seeds/PositionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class PositionSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        $faker = Faker::create();
+        for ($i = 1; $i <= 30; $i++) {
+
+            // insert data ke table pegawai menggunakan Faker
+            DB::table('positions')->insert([
+                
+                'branch_id' => $faker->numberBetween($min = 1, $max = 4), // 8567
+                'nama' => $faker->jobTitle,
+                'salary' => $faker->numberBetween($min = 2300000, $max = 3100000), // 8567,
+                ]);
+        }
+    }
+
+}
