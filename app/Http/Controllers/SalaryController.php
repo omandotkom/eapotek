458f63b7352559f5ac44a9d480d49db490be06e7
Khalid Abdurrahman <omandotkom@gmail.com>
2019-05-27 20:41:24 +0700

    create showCetakSlipGajiView

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -1,14 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Branch;
 use Illuminate\Http\Request;
 
-class SalaryController extends Controller
-{
-    public function showSalaryInputView(){
+class SalaryController extends Controller {
+
+    public function showSalaryInputView() {
         $branches = Branch::all();
-        return view('salary.salary',['branches'=>$branches]);
-    
-         }
+        return view('salary.salary', ['branches' => $branches]);
+    }
+
+    public function showCetakSlipGajiView() {
+        $branches = Branch::all();
+        return view('salary.slip', ['branches' => $branches]);
+    }
+
 }
