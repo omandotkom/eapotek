7ceccae8449bc9454cc98db6d0b712b258de3fbe
Khalid Abdurrahman <omandotkom@gmail.com>
2019-06-08 23:49:48 +0700

    remove unused comments

--- routes/web.php
+++ routes/web.php
@@ -10,71 +10,7 @@
   | contains the "web" middleware group. Now create something great!
   |
  */
-/*
-use App\Supplier;
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/dashboard/owner', function () {
-    return view('dashboard.dashowner');
-})->middleware('auth');
-Route::get('/dashboard/branchhead', function() {
-    return view('dashboard.dashbranchhead');
-})->middleware('auth');
-
-Route::get('/dashboard/branchworker', function() {
-    return view('dashboard.dashbranchworker');
-})->middleware('auth');
-
-Route::get('/login/checklogin', 'Auth\LoginController@redirectTo')->name('dashboard');
-
-Route::get('/input/obat', 'MedicineController@showInputObatView')->name('inputObat');
-Route::post('/input/obat/store', 'MedicineController@store')->name('storeObat');
-
-  Route::get('/input/supplier',function(){
-  return view('input.supplier');
-  })->name('inputSupplier'); 
-Route::get('/input/supplier', 'SupplierController@showInputSupplierView')->name('inputSupplier');
-Route::get('/input/karyawan', 'WorkerController@showWorkerView' )->name('inputKaryawan');
-Route::post('/input/karyawan/store','WorkerController@store');
-Route::get('/input/supplyobat', 'NoteSupplierController@showNoteSupplierView')->name('inputSupplyObat');
-Route::post('/input/supplyobat/store','NoteSupplierController@store');
-Route::post('/supplier', function() {
-    $data = $request->all();
-    $supplier = new Supplier($data);
-    $result = $transaction->isValid();
-    echo $result;
-})->name('storeSupplier');
-//Route::get('/supplier/branch/{branch_id}', 'SupplierController@showSupplierbyBranch')->name('getSupplierbyBranchID');
-
-Route::post('/supplier/store', 'SupplierController@store');
-
-Route::get('/absensi', 'AttendanceController@showAttendanceView')->name('absensi');
-Route::post('/absensi/store','AttendanceController@store');
-Route::get('/view/obat', 'MedicineController@showAllMedicine')->name('viewObat');
-
-//Route::get('/view/supplier', 'SupplierController@showAllSupplier')->name('viewSupplier');
-Route::get('/view/supplier/branch/{branch_id}','SupplierController@showSupplierbyBranch')->name('viewSupplier');
-Route::get('/view/karyawan', 'WorkerController@showAllWorker')->name('viewKaryawan');
-
-Route::get('/view/supplyobat', function() {
-    return view('view.supplyobat');
-})->name('viewSupplyObat');
-
-Route::get('/transaction/add','TransactionController@showTransactionPage')->name("transactionAdd");
-
-Route::get('/input/salary', 'SalaryController@showSalaryInputView')->name('inputSalary');
-
-Route::get('/view/obat/branch/{branch_id}','MedicineController@byBranchID');
-
-Route::get('/view/slipgaji','SalaryController@showCetakSlipGajiView')->name('slipGaji');
- * 
- */
  Route::get('/', function () {
         return view('auth.login');
     });
@@ -116,6 +52,7 @@ Route::get('/view/slipgaji','SalaryController@showCetakSlipGajiView')->name('sli
     Route::post('/input/obat/store', 'MedicineController@store')->name('storeObat');
     Route::get('/view/obat/branch/{branch_id}','MedicineController@showSupplierbyBranch')->name('viewObat');
     Route::get('/view/obat/search/branch/{branch_id}','MedicineController@loadData');
+    Route::get('/view/obat/search/branch/{branch_id}/{namaobat}','MedicineController@searchName')->name('viewObatByName');
     
     Route::get('/input/karyawan', 'WorkerController@showWorkerView' )->name('inputKaryawan');
     //Route::get('/view/karyawan', 'WorkerController@showAllWorker')->name('viewKaryawan');
