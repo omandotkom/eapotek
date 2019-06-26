d359af13da0c3b26e88522010f05ea0b06f33ed1
Khalid Abdurrahman <omandotkom@gmail.com>
2019-06-26 10:44:54 +0700

    Add updateSalary
    updateSalary used to update position's salary, it takes branch_id as param

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -5,18 +5,30 @@ namespace App\Http\Controllers;
 use App\Branch;
 use Illuminate\Http\Request;
 use App\Position;
+use Response;
 class SalaryController extends Controller {
 
     public function showSalaryInputView($branch_id) {
         $branches = Branch::all();
         $positions = Position::where('branch_id',$branch_id)->get();
-        return view('salary.salary', ['branches' => $branches,'positions' => $positions]);
+        return view('salary.salary', ['branches' => $branches,'positions' => $positions, 'branch_id' => $branch_id]);
     
     }
 
     public function showCetakSlipGajiView() {
         $branches = Branch::all();
         return view('salary.slip', ['branches' => $branches]);
+    }
+    
+    public function updateSalary(Request $request){
+        $id = $request->id;
+        $salary = $request->salary;
+        $position = Position::find($id);
+        $position->salary = $salary;
+        $position->save();
+        return Response::json([
+            'action' => 'update_salary'
+                ], 201); // Status code here
     }
 
 }
