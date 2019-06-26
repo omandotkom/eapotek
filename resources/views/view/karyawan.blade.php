18664ce2345cfd5b1cac025e94f436c677c8bcfb
Khalid Abdurrahman <omandotkom@gmail.com>
2019-06-26 13:18:07 +0700

    Replace Rincian Supplier to Rincian  Karyawan
    there's a typo in detil pop up, it supposed to be Rincian Karyawan not Rincian Supplier

--- resources/views/view/karyawan.blade.php
+++ resources/views/view/karyawan.blade.php
@@ -81,7 +81,7 @@
                             <div class="modal-dialog modal-dialog-centered" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalCenterTitle">Rincian Supplier</h5>
+                                        <h5 class="modal-title" id="exampleModalCenterTitle">Rincian Karyawan</h5>
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
                                         </button>
