c34e3d095323f85827998ea693d933afac5fc1fc
Khalid Abdurrahman <omandotkom@gmail.com>
2019-06-26 15:52:28 +0700

    remove jquery now using app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
 
         <!-- Scripts -->
         <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-        --><script src="{{asset('js/app.js')}}"></script>
+        -->
+        <script src="{{asset('js/app.js')}}"></script>
         <script src="{{asset('js/redirector.js')}}" ></script>
         <script src="{{asset('js/bootbox.min.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
