4e2ce000e8b7c005615d737714855c91a6f45203
Khalid Abdurrahman <omandotkom@gmail.com>
2019-06-29 08:12:53 +0700

    add search(branch_id, id)
    to support update/delete

--- resources/views/input/supplyobat.blade.php
+++ resources/views/input/supplyobat.blade.php
@@ -38,7 +38,7 @@ $(document).ready(function(){
                             </div>
                             <input type="text" class="form-control" id="idSupply" aria-describedby="basic-addon2" aria-label="Text input with checkbox">
                             <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" id="cariKode" type="button">Cari Kode</button>
+                                <button class="btn btn-outline-secondary" id="cariKode" onclick="search();" type="button">Cari Kode</button>
                             </div>
                         </div>
 
@@ -66,10 +66,86 @@ $(document).ready(function(){
                         </div>
                         <div class="text-right">
                             <button type="submit" onclick="onSubmitClicked();" id="send_form" class="btn btn-outline-success mr-2">Simpan</button>
-                            <button type="submit" onclick="onSubmitClicked();" id="edit_form" class="btn btn-outline-info mr-2">Edit</button>                
+                            <button type="submit" onclick="onUpdateClicked();" id="edit_form" class="btn btn-outline-info mr-2">Edit</button>                
                             <button type="submit" onclick="onSubmitClicked();" id="delete_form" class="btn btn-outline-danger">Delete</button>
                         </div>
                         <script>
+                        function onUpdateClicked(){
+                            $("#edit_form").html('Memperbarui...');
+                                axios.post('http://homestead.test/input/supplyobat/update', {
+                                    id: jQuery('#idSupply').val(),
+                                    branch_id: '{{$cabang->id}}',
+                                    supplier_id: jQuery('#namaSupplier').val(),
+                                    deskripsi: jQuery('#deskripsi').val(),
+                                    totalbiaya: jQuery('#totalBiaya').val(),
+                                    tanggalsupply: jQuery('#tanggal').val()
+
+                                })
+                                        .then(function (response) {
+                                            toastr.options = {
+                                                "closeButton": false,
+                                                "debug": false,
+                                                "newestOnTop": false,
+                                                "progressBar": false,
+                                                "positionClass": "toast-top-center",
+                                                "preventDuplicates": false,
+                                                "onclick": null,
+                                                "showDuration": "300",
+                                                "hideDuration": "1000",
+                                                "timeOut": "5000",
+                                                "extendedTimeOut": "1000",
+                                                "showEasing": "swing",
+                                                "hideEasing": "linear",
+                                                "showMethod": "fadeIn",
+                                                "hideMethod": "fadeOut"
+                                            };
+                                            Command: toastr["success"]("Berhasil menyimpan data", "Berhasil");
+
+                                            console.log(response);
+                                            $("#edit_form").html('Edit');
+                                        })
+                                        .catch(function (error) {
+                                            toastr.error("Gagal menyimpan data", "Kesalahan");
+                                            $("#edit_form").html('Edit');
+                                            console.log(error);
+                                        });
+                        }
+                        function search(){
+                            $("#cariKode").html('Mencari...');
+                            var url = 'http://homestead.test/input/supplyobat/searchbyid/' + '{{$cabang->id}}' + '/' + $("#idSupply").val();
+                            console.log (url);
+                                axios.get(url)
+                                        .then(function (response) {
+                                            console.log(response);
+                                            toastr.options = {
+                                                "closeButton": false,
+                                                "debug": false,
+                                                "newestOnTop": false,
+                                                "progressBar": false,
+                                                "positionClass": "toast-top-center",
+                                                "preventDuplicates": false,
+                                                "onclick": null,
+                                                "showDuration": "300",
+                                                "hideDuration": "1000",
+                                                "timeOut": "5000",
+                                                "extendedTimeOut": "1000",
+                                                "showEasing": "swing",
+                                                "hideEasing": "linear",
+                                                "showMethod": "fadeIn",
+                                                "hideMethod": "fadeOut"
+                                            };
+                                            $("#namaSupplier").val(response.data.supplier_id);
+                                            $("#deskripsi").val(response.data.deskripsi);
+                                            $("#totalBiaya").val(response.data.totalbiaya);
+                                            $("#tanggal").val(response.data.tanggalsupply);
+                                            $("#cariKode").html('Cari Kode');
+                                        })
+                                        .catch(function (error) {
+                                            toastr.error("Data dengan kode tersebut tidak ditemukan", "Kesalahan");
+                                            $("#cariKode").html('Cari Kode');
+                                            console.log(error);
+                                        });    
+                        }
                             async function onSubmitClicked() {
 
                                 $("#send_form").html('Menyimpan...');
