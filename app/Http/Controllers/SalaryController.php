e1e01f502e5eb10cef584b8e9a2cafca55e2ab1b
Khalid Abdurrahman <omandotkom@gmail.com>
2019-07-01 23:15:32 +0700

    add LoadData
    modify load data & slip gaji nik

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -6,6 +6,9 @@ use App\Branch;
 use Illuminate\Http\Request;
 use App\Position;
 use Response;
+use Auth;
+use App\Attendance;
+use App\Worker;
 class SalaryController extends Controller {
 
     public function showSalaryInputView($branch_id) {
@@ -16,8 +19,8 @@ class SalaryController extends Controller {
     }
 
     public function showCetakSlipGajiView() {
-        $branches = Branch::all();
-        return view('salary.slip', ['branches' => $branches]);
+        $workers = Worker::where('branch_id' , Auth::user()->worker->branch_id)->get();
+        return view('salary.slip', ['workers' => $workers]);
     }
     
     public function updateSalary(Request $request){
@@ -31,4 +34,14 @@ class SalaryController extends Controller {
                 ], 201); // Status code here
     }
 
+    public function loadData(Request $request){ 
+        //$attendances = Attendance::where('nik',$request->nik)->whereBetween('tanggal',[$request->from,$request->to])->get();
+        $masuk = Attendance::where('nik',$request->nik)->where("keterangan","Masuk")->whereBetween('tanggal',[$request->from,$request->to])->count();
+        $izin = Attendance::where('nik',$request->nik)->where("keterangan","Izin")->whereBetween('tanggal',[$request->from,$request->to])->count();
+        $alfa = Attendance::where('nik',$request->nik)->where("keterangan","Alpha")->whereBetween('tanggal',[$request->from,$request->to])->count();
+        $worker = Worker::with('position')->where('nik',$request->nik)->first(); 
+    return response()->json(['worker'=>$worker,'masuk'=>$masuk,'izin' => $izin,'alpha'=>$alfa]);      
+//        return response(['attendances' => $attendances, 'worker' => $worker]);
+        
+    }
 }
