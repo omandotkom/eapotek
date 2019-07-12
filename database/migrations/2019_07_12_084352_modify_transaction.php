afb79324b5c5d919833b2b5828b99c868de791dd
Khalid Abdurrahman <omandotkom@gmail.com>
2019-07-12 16:41:21 +0700

    add tanggal column
    tanggal used to defince when was transaction fired

---
+++ database/migrations/2019_07_12_084352_modify_transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyTransaction extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->date('tanggal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
